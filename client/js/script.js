4cd05e5200ab90b824916fd0e81a232ccb87990b
BrycePearce <bpearce@angelo.edu>
2017-01-18 20:37:38 -0600

    Created event handlers for all results. Fixed other li issues, added DVD release date

--- client/js/script.js
+++ client/js/script.js
@@ -8,39 +8,42 @@ document.addEventListener('DOMContentLoaded', function () {
     var key = '&api_key=f016113c794da0ca4fc69f2cbeaca136';
     var key2 = 'f016113c794da0ca4fc69f2cbeaca136';
 
+
     document.getElementById("btn").addEventListener("click", function () {
-        var input = $('#movie').val();
+        var input = document.getElementById('movie').value;
 
         fetch(posterQuery + input + key)
             .then(function (response) {
                 response.json().then(function (json) {
-                    console.log(json);
+                    //console.log(json);
 
                     json.results.forEach(function (result) {
-                        //console.log(result.original_title);
                         //create li element, append results as text and output to our UL
-                        var node = document.createElement("LI");
+                        var li = document.createElement("LI");
+                        li.onclick = showClick.bind(this, result); //(attaches onClick to each li, so we can get the whole object including ID)
                         var textnode = document.createTextNode(result.original_title);
-                        node.appendChild(textnode);
-                        document.getElementById("possibleResults").appendChild(node);
+                        li.appendChild(textnode);
+                        document.getElementById("possibleResults").appendChild(li);
                     }, this);
                     document.getElementById("poster").src = "https://image.tmdb.org/t/p/w500" + json.results[0].poster_path;
                 });
             });
-            //write clicked text to the output
+        //write clicked text to the output
         document.getElementById("possibleResults").addEventListener("click", function (e) {
             // e is the mouseEvent, e.target is the user selected li, e.target.innerText is the innertext of the li
             document.getElementById("tempOut").innerText = e.target.innerText;
-            console.log(e);
-            console.log(e.target);
         });
-        
-        /*fetch(dateQuery + input + key2)
+    });
+
+    function showClick(result) {
+        movie.value = result.original_title;
+
+        //dvd query
+        fetch(dateQuery + result.id + "/release_dates?api_key=" + key2)
             .then(function (response) {
                 response.json().then(function (json) {
-                    console.log(json);
-                    //document.getElementById("date").innerText = dateQuery + input + '/images?api_key=' + key + '&language=en-US';
+                    document.getElementById("date").innerText = json.results[0].release_dates[0].release_date;
                 });
-            });*/
-    });
+            });
+    };
 });
