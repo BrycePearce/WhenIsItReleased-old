0b9335f1141b8168f34c5f964d95588385b83560
BrycePearce <bpearce@angelo.edu>
2017-02-06 02:00:34 -0600

    Fixed routing issues, redirects now point back to the main site. Various CSS changes.

--- express.js
+++ express.js
@@ -8,6 +8,10 @@ app.set('port', process.env.PORT || 1337);
 //Sets root folder as src, used to import Static assets, such as scripts/images/etc. "/static" prevents clashes with other routes.
 app.use("/", express.static(path.resolve(__dirname, 'build/')));
 
+app.get('*', function(req, res) {
+  res.sendFile(path.resolve(__dirname + '/build/index.html'));
+});
+
 //always last so you can make sure everything else is loaded before accepting connections.
 app.listen(app.get('port'), function () {
   console.log("Express started on port: " + app.get('port'));
--- src/app.js
+++ src/app.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { render } from 'react-dom';
 import Search from './Search'
 import Details from './Details';
-import { Router, Route, browserHistory } from 'react-router'
+import { Router, Route, browserHistory, Redirect } from 'react-router'
 
 render((
   <Router history={browserHistory}>
     <Route path="/" component={Search} />
     <Route path="/details/:id" component={Details} />
+    <Redirect from="*" to="/"/>
 
   </Router>
 
