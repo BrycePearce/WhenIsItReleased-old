494d6eaeb5416d4a7e41e48e684adee7443007ef
BrycePearce <bpearce@angelo.edu>
2017-02-07 22:44:58 -0600

    cleared up api issue, set precedence for web releases, then BD

--- src/Details.js
+++ src/Details.js
@@ -10,36 +10,43 @@ class Details extends React.Component {
         this.props.onChange(this.textInput.value);
     }
 
-
     render() {
-        let apiPath = this.props.location.state.show;
-        let releaseDate = apiPath.release_dates;
+        /*
+        //handle external requests
+        if (this.state.props == undefined) {
+            
+        }*/
+        const apiPath = this.props.location.state.show;
+        const releasePath = apiPath.release_dates;
+        let releaseDate = "";
+
 
         if (apiPath.backdrop_path != undefined) {
             document.body.style.backgroundImage = "url('https://image.tmdb.org/t/p/w1920" + this.props.location.state.show.backdrop_path + "')";
         } else { console.log("backdrop not found"); } //TODO: set this to default background later
-
-        if (releaseDate.results.length > 0) {
-            for (var i = 0; i < releaseDate.results.length; i += 1) {
-                if (releaseDate.results[i].iso_3166_1 == "US") {
+        //find the date for US release
+        if (releasePath.results.length > 0) {
+            for (var i = 0; i < releasePath.results.length; i += 1) {
+                if (releasePath.results[i].iso_3166_1 == "US") {
                     //now find the type: 4 digital, 5 physical
-                    for (var q = 0; q < releaseDate.results[i].release_dates.length; q += 1) {
-                        if (releaseDate.results[i].release_dates[q].type == "3" || releaseDate.results[i].release_dates[q].type == "4") {
-                            console.log("dvd release found");
-                            console.log(releaseDate.results[i].release_dates[q].release_date);
-                            console.log(apiPath);
-                            //releaseDate = releaseDate.results[i].release_dates[q].release_date;
+                    for (var q = 0; q < releasePath.results[i].release_dates.length; q += 1) {
+                        if (releasePath.results[i].release_dates[q].type == "4") {
+                            releaseDate = moment(releasePath.results[i].release_dates[q].release_date).format('MMMM Do YYYY');
+                        }
+                        else if (releasePath.results[i].release_dates[q].type == "5") {
+                            releaseDate = moment(releasePath.results[i].release_dates[q].release_date).format('MMMM Do YYYY');
+                        }
+                        else {
+                            releaseDate = "No US release date available";
                         }
                     }
+                    break;
                 }
             }
-            //console.log(releaseDate.results[0].release_dates.length);
-            // releaseDate = moment(apiPath.release_dates.results[8].release_dates[1].release_date).format('MMMM Do YYYY');
         } else {
             releaseDate = "Date Unavailable";
         }
-        //console.log(this.props.location.state.show.release_dates.results[8].release_dates[1].release_date);
-        //console.log(this.props.location.state.show.release_dates.results[8].release_dates[1].release_date);
+
         return (
             <div className="resultContainer">
                 <div className="detailsHeader">
