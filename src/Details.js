c4cabd947cc5d169107ccd32c9745afdc4e2b300
m4tta <matt@fizzled.me>
2017-02-08 16:53:24 -0800

    direct load

--- src/Details.js
+++ src/Details.js
@@ -3,26 +3,44 @@ import moment from 'moment';
 import Search from './Search';
 import Logo from './static/logo.png';
 import SearchBox from './SearchBox';
+import Key from './Key';
 
 class Details extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      show: this.props.location.state ? this.props.location.state.show : null
+    };
+
+    if (!this.state.show) {
+      fetch('http://api.themoviedb.org/3/movie/' + this.props.params.id + "?api_key=" + Key + '&append_to_response=release_dates')
+          .then((response) => {
+              response.json().then((json) => {
+                this.setState({show: json});
+              });
+          });
+    }
+
+  }
 
     change() {
         this.props.onChange(this.textInput.value);
     }
 
     render() {
-        /*
-        //handle external requests
-        if (this.state.props == undefined) {
-            
-        }*/
-        const apiPath = this.props.location.state.show;
+        if (!this.state.show) {
+          return <div>Loading...</div>
+        }
+
+        const apiPath = this.state.show;
         const releasePath = apiPath.release_dates;
         let releaseDate = "";
 
 
         if (apiPath.backdrop_path != undefined) {
-            document.body.style.backgroundImage = "url('https://image.tmdb.org/t/p/w1920" + this.props.location.state.show.backdrop_path + "')";
+            document.body.style.backgroundImage = "url('https://image.tmdb.org/t/p/w1920" + this.state.show.backdrop_path + "')";
         } else { console.log("backdrop not found"); } //TODO: set this to default background later
         //find the date for US release
         if (releasePath.results.length > 0) {
